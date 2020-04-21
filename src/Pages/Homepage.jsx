cb84a3bfc56754c9412e749ae0c3d58ec7975736
angelopoole <angelo.poole@outlook.com>
2020-04-21 15:21:15 -0400

    changing up styles

---
+++ src/Pages/Homepage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Homepage = () => {
+
+    console.log("HelloQ")
+
+    return (
+        <div>
+            <p> hello </p>
+        </div>
+    )
+}
+
+export default Homepage
