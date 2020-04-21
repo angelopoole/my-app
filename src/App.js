0cbe89721f1d12a14f1e12db96957d1f498cd47e
angelopoole <angelo.poole@outlook.com>
2020-04-21 16:28:35 -0400

    working on implementing cards

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import Homepage from './Pages/Homepage'
 
 function App() {
--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,13 +1,42 @@
 import React from 'react'
+import { Card, CardGroup } from 'semantic-ui-react'
+import "../styles/homepage.scss"
 
 const Homepage = () => {
 
-    console.log("HelloQ")
+    const items = [
+        {
+            image: 'https://avatars3.githubusercontent.com/u/9421706?s=460&u=ad5e3e5bad96970aa57444b19f4b5382fef54e7a&v=4',
+            header: 'Bloom',
+            description: 'an interactive js soundboard app',
+            href: "ill put the link here when it's deployed",
+            color: "red"
+        },
+        {
+            header: "StarWars Bounty Guild",
+            description: "A rails monolith app for those who want to live like boba-the-fet"
+        }
+    ]
+
+    console.log("Hello")
 
     return (
-        <div>
-            <p> hello </p>
+        <div className="back">
+            <img className="image" src={"https://avatars3.githubusercontent.com/u/9421706?s=460&u=ad5e3e5bad96970aa57444b19f4b5382fef54e7a&v=4"} alt="" />
+            <p> hello! My name is angelo and im an aspiring software engeneer. I've recently graduated from the Flatiron school for software engeneering and have been looking to improve upon myself everyday since graduation! </p>
+
+
+            <div className="ProjectShowcase">
+
+            <Card.Group items={items}>
+            </Card.Group>
+
+                
+            </div>
         </div>
+
+
+
     )
 }
 
